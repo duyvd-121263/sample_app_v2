3f64d50bd80f17d0a9bbd10f09ddca4d717722ff
duyvd <vu.duc.duy@sun-asterisk.com>
2022-05-24 14:37:40 +0700

    Chapter 12

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
     root "static_pages#home"
     resources :users
     resources :account_activations, only: :edit
+    resources :password_resets, except: %i(index show destroy)
   end
 end
