d1392759de562954e5775267291d495e09138c9e
duyvd <vu.duc.duy@sun-asterisk.com>
2022-05-25 16:44:40 +0700

    Chapter 14

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,9 @@ class UsersController < ApplicationController
     @users = User.paginate page: params[:page], per_page: Settings.users_controller.size.per_page
   end
 
-  def show; end
+  def show
+    @microposts = @user.microposts.paginate page: params[:page], per_page: Settings.users_controller.size.per_page
+  end
 
   def new
     @user = User.new
@@ -46,6 +48,20 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
+  def following
+    @title = "Following"
+    @user = User.find_by(id: params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render "show_follow"
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find_by(id: params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render "show_follow"
+  end
+
   private
 
   def user_params
@@ -56,14 +72,6 @@ class UsersController < ApplicationController
   def find_user
     @user = User.find_by id: params[:id]
     redirect_to root_path unless @user
-  end
-
-  def logged_in_user
-    return if logged_in?
-
-    store_location
-    flash[:danger] = t ".login_warning."
-    redirect_to login_url
   end
 
   def correct_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: Relationship.name,
+            foreign_key: :follower_id, dependent: :destroy
+  has_many :passive_relationships, class_name: Relationship.name,
+            foreign_key: :followed_id, dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
   VALID_EMAIL_REGEX = Settings.user.email_regex
 
   validates :email, presence: true,
@@ -70,6 +78,22 @@ class User < ApplicationRecord
 
   def password_reset_expired?
     reset_sent_at < Settings.user.time_password_expired.hours.ago
+  end
+
+  def feed
+    Micropost.where user_id: (following_ids << id)
+  end
+
+  def follow other_user
+    following << other_user
+  end
+
+  def unfollow other_user
+    following.delete other_user
+  end
+
+  def following? other_user
+    following.include? other_user
   end
 
   private
