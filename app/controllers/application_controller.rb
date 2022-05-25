4eecfab92b249649f8ca95bdb70f059e67a3b541
duyvd <vu.duc.duy@sun-asterisk.com>
2022-05-25 11:13:24 +0700

    Chapter 13

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  include Pagy::Backend
+
   before_action :set_locale
 
   protect_from_forgery with: :exception
@@ -13,5 +15,13 @@ class ApplicationController < ActionController::Base
 
   def default_url_options
     {locale: I18n.locale}
+  end
+
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = t ".login_warning."
+    redirect_to login_url
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,16 @@ User.create!(name: "Example User",
               activated: true,
               activated_at: Time.zone.now)
 end
+
+users = User.order(:created_at).take(6)
+30.times do |n|
+    content = "hello"
+    users.each { |user| user.microposts.create!(content: content) }
+end
+
+users = User.all
+user = users.first
+following = users[2..20]
+followers = users[3..15]
+following.each{|followed| user.follow(followed)}
+followers.each{|follower| follower.follow(user)}
